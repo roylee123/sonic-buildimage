bda084b8aa6f54cea8f7bd82ab5854622f889cd4
roy_lee <roy_lee@accton.com>
2019-10-18 15:12:27 +0800

    Update for R0C. Add checking if i2c adatpor flipped. Add ucd90160, the power monitor. Signed-off-by: roy_lee <roy_lee@accton.com>

--- platform/broadcom/sonic-platform-modules-accton/as7315-27xb/utils/accton_as7315_util.py
+++ platform/broadcom/sonic-platform-modules-accton/as7315-27xb/utils/accton_as7315_util.py
@@ -232,24 +232,81 @@ mknod =[
 'echo ym2401  0x58 > /sys/bus/i2c/devices/i2c-12/new_device',
 'echo as7315_27xb_psu1  0x53 > /sys/bus/i2c/devices/i2c-13/new_device',
 'echo ym2401  0x5b > /sys/bus/i2c/devices/i2c-13/new_device',
-'echo as7315_fan  0x66 > /sys/bus/i2c/devices/i2c-50/new_device',
+'echo as7315_fan  0x66 > /sys/bus/i2c/devices/i2c-9/new_device',
 'echo lm75  0x49 > /sys/bus/i2c/devices/i2c-51/new_device',
 'echo lm75  0x4a > /sys/bus/i2c/devices/i2c-52/new_device',
 'echo lm75  0x4c > /sys/bus/i2c/devices/i2c-53/new_device',
+'echo ucd90160 0x34 > /sys/bus/i2c/devices/i2c-11/new_device',
 ]
+
+mknod2 =[   
+'echo pca9548  0x76 > /sys/bus/i2c/devices/i2c-1/new_device',
+'echo pca9548  0x74 > /sys/bus/i2c/devices/i2c-3/new_device',
+'echo pca9548  0x72 > /sys/bus/i2c/devices/i2c-0/new_device',
+'echo pca9548  0x70 > /sys/bus/i2c/devices/i2c-18/new_device',
+'echo pca9548  0x71 > /sys/bus/i2c/devices/i2c-19/new_device',
+'echo pca9548  0x73 > /sys/bus/i2c/devices/i2c-20/new_device',
+
+'echo as7315_cpld1  0x63 > /sys/bus/i2c/devices/i2c-8/new_device',
+'echo as7315_cpld2  0x64 > /sys/bus/i2c/devices/i2c-7/new_device',
+
+'echo 24cxb04  0x57 > /sys/bus/i2c/devices/i2c-4/new_device',
+'echo as7315_27xb_psu2  0x50 > /sys/bus/i2c/devices/i2c-12/new_device',
+'echo ym2401  0x58 > /sys/bus/i2c/devices/i2c-12/new_device',
+'echo as7315_27xb_psu1  0x53 > /sys/bus/i2c/devices/i2c-13/new_device',
+'echo ym2401  0x5b > /sys/bus/i2c/devices/i2c-13/new_device',
+'echo as7315_fan  0x66 > /sys/bus/i2c/devices/i2c-9/new_device',
+'echo lm75  0x49 > /sys/bus/i2c/devices/i2c-51/new_device',
+'echo lm75  0x4a > /sys/bus/i2c/devices/i2c-52/new_device',
+'echo lm75  0x4c > /sys/bus/i2c/devices/i2c-53/new_device',
+'echo ucd90160 0x34 > /sys/bus/i2c/devices/i2c-11/new_device',
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
        
-def i2c_order_check():    
-    return 0
+def i2c_order_check():
+    # i2c bus 0 and 1 might be installed in different order.
+    # Here check if 0x76 is exist @ i2c-0
+    tmp = "echo pca9548 0x76 > /sys/bus/i2c/devices/i2c-0/new_device"
+    status, output = log_os_system(tmp, 0)
+    if not device_exist():
+        order = 1
+    else:
+        order = 0
+    tmp = "echo 0x76 > /sys/bus/i2c/devices/i2c-0/delete_device"
+    status, output = log_os_system(tmp, 0)
+    return order    
                      
 def device_install():
     global FORCE
-    
-    for i in range(0,len(mknod)):
+   
+    order = i2c_order_check()
+    if order:
+        nodes = mknod2
+    else:
+        nodes = mknod
+    for i in range(0,len(nodes)):
         #for pca954x need times to built new i2c buses            
-        if mknod[i].find('pca954') != -1:
+        if nodes[i].find('pca954') != -1:
            time.sleep(1)         
            
-        status, output = log_os_system(mknod[i], 1)
+        status, output = log_os_system(nodes[i], 1)
         if status:
             print output
             if FORCE == 0:                
@@ -269,8 +326,12 @@ def device_install():
     
 def device_uninstall():
     global FORCE
-    
-    status, output =log_os_system("ls /sys/bus/i2c/devices/1-0076", 0)
+
+    order = i2c_order_check()
+    if order == 0:
+        nodelist = mknod
+    else:
+        nodelist = mknod2
     
     for i in range(0,len(sfp_map)):
         target = "/sys/bus/i2c/devices/i2c-"+str(sfp_map[i])+"/delete_device"
@@ -279,8 +340,6 @@ def device_uninstall():
             print output
             if FORCE == 0:            
                 return status
-       
-    nodelist = mknod
            
     for i in range(len(nodelist)):
         target = nodelist[-(i+1)]
